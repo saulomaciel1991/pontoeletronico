34fc2ec54079cea634a4eb7ad39b0a305c05f8c5
caroltavares1 <caroltavares.ts@gmail.com>
2022-12-01 14:44:17 -0300

    Tela de login parcialmente feita

--- ponto-eletronico/src/app/app.module.ts
+++ ponto-eletronico/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PoModule } from '@po-ui/ng-components';
 import { PoTemplatesModule } from '@po-ui/ng-templates';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+
+//componentes
+import { PoPageLoginModule } from '@po-ui/ng-templates';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     PoModule,
-    PoTemplatesModule
+    PoTemplatesModule,
+    AppRoutingModule,
+    PoPageLoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ponto-eletronico/src/app/login/login.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { PoPageLoginLiterals } from '@po-ui/ng-templates';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+
+export class LoginComponent implements OnInit {
+
+  customLiterals: PoPageLoginLiterals = {
+    registerUrl: "Cadastrar Usuário"
+  };
+
+  loading: boolean = false;
+  loginErrors: string[] = [];
+  passwordErrors: string[]= [];
+
+
+
+  constructor() { }
+
+  checkLogin(formData: any) {
+    this.loading = true;
+
+    if (formData.login === 'devpo' && formData.password === '1986') {
+      this.passwordErrors = [];
+      //this.exceededAttempts = 0;
+      this.loginErrors = [];
+
+      setTimeout(() => {
+        
+      }, 3000);
+    } else {
+      this.loading = false;
+      this.passwordErrors = ['Senha e/ou usuário inválido, verifique e tente novamente.'];
+      this.loginErrors = ['Senha e/ou usuário inválido, verifique e tente novamente.'];
+    }
+  }
+
+  
+  passwordChange() {
+    if (this.passwordErrors.length) {
+      this.passwordErrors = [];
+    }
+  }
+
+  loginChange() {
+    if (this.loginErrors.length) {
+      this.loginErrors = [];
+    }
+  }
+
+
+  ngOnInit(): void {
+  }
+
+}
