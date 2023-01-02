68d175317b995c9a4fa2fc38a8038600014617ee
Saulo <saulomcm@gmail.com>
2023-01-02 11:39:51 -0300

    Ativa a guarda na autenticação

--- ponto-eletronico/src/app/app-routing.module.ts
+++ ponto-eletronico/src/app/app-routing.module.ts
@@ -3,12 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { FormComponent } from './user/form/form.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {
-    path: '', component: HomeComponent, /* canActivate: [AuthGuard], */
+    path: '', component: HomeComponent, canActivate: [AuthGuard],
     children: [
       { path: "user", loadChildren: () => import("./user/user.module").then(m => m.UserModule) }
     ]
